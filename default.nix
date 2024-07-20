beb18ce56722e192c54326d21a343dfdcc443678
Mag1cByt3s <ppeinecke@protonmail.com>
2024-07-20 12:48:18 +0200

    push latest state

--- default.nix
+++ default.nix
@@ -1,31 +1,44 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, python3
-, buildPythonPackage
-, python3Packages
-}:
+{ inputs, lib, ... }@args: self: super: rec {
 
-buildPythonPackage rec {
-  pname = "pyserver";
-  version = "0.0.1";
+  pyserver = super.python3Packages.buildPythonApplication rec {
 
-  src = fetchFromGitHub {
-    owner = "Red-Flake";
-    repo = "pyserver";
-    rev = "2f7dfda9808074a7fdac149abe8d3242e7a3ca19";
-    hash = "sha256-WlZShvLkxl+gxL+iu7IQRQMgx59kZUgR6d6OGStfxS0=";
-  };
+    pname = "pyserver";
+    version = "0.0.1";
 
-  propagatedBuildInputs = with python3Packages; [
-    colorlog
-  ];
+    # Pull source from a Git server. Optionally select a specific `ref` (e.g. branch),
+    # or `rev` revision hash.
+    src = super.fetchFromGitHub rec {
+      inherit pname version;
+      name = pname;
+      rev = version;
+      owner = "Red-Flake";
+      repo = "pyserver";
+      sha256 = "sha256-WlZShvLkxl+gxL+iu7IQRQMgx59kZUgR6d6OGStfxS0=";
+    };
 
-  meta = with lib; {
-    description = "A simple multithreaded HTTP server with fancy directory listing and logging";
-    homepage = "https://github.com/Red-Flake/pyserver";
-    license = licenses.mit;
-    maintainers = with maintainers; [ Mag1cByt3s ];
-    platforms = platforms.all;
+    # If no `checkPhase` is specified, `python setup.py test` is executed
+    # by default as long as `doCheck` is true (the default).
+    # I want to run my tests in a different way:
+    checkPhase = ''
+      python -m unittest $(find tests -name "*.py")
+    '';
+
+    installPhase = ''install -Dm755 pyserver.py $out/bin/pyserver'';
+    preFixup = ''
+      makeWrapperArgs+=(--prefix PATH : ${args.lib.makeBinPath [
+        # Hard requirements
+        super.python3Packages.colorlog
+      ]})
+    '';
+
+    # Meta information for the package
+    meta = with lib; {
+      description = "A simple multithreaded HTTP server with fancy directory listing and logging";
+      homepage = "https://github.com/Red-Flake/pyserver";
+      license = licenses.mit;
+      maintainers = with maintainers; [ Mag1cByt3s ];
+      platforms = lib.platforms.all;
+      mainProgram = "pyserver";
+    };
   };
 }
---
+++ src/pyserver_Mag1cByt3s/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='pyserver',
+    version='0.0.1',
+    scripts=['pyserver.py',],
+)
